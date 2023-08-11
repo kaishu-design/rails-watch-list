d8457a2950f7a84e7662a6d35a3adab7310386de
exec zsh <kaishu0520@gmail.com>
2023-08-11 12:44:18 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'lists#inde'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
-  resources :bookmarks, only: [:destoroy]
+  resources :bookmarks, only: :destroy
 end
